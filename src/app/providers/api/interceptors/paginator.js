68eaf91fe6836439138ae3fd18c1011307ece176
brunoziie <eu@brunoziie.com>
2019-11-12 14:49:55 -0300

    add alternative formats to paginator interceptor

--- src/app/providers/api/interceptors/paginator.js
+++ src/app/providers/api/interceptors/paginator.js
@@ -4,6 +4,14 @@ RESTFullResource.intercept('response', (res) => {
 
 	if (data && has('page') && has('lastPage') && has('total') && has('perPage') && Array.isArray(data.data)) {
 		res.response = new PaginatedResponse(data);
+	} else if (data && has('pagination') && has('result')) {
+		res.response = new PaginatedResponse(Object.assign({}, data.pagination, { data: data.result }));
+	} else if (data && has('status')){
+		if (data.status === 'OK') {
+			res.response = data.result || null;
+		} else {
+			res.response = data.error;
+		}
 	}
 
 	return res;
