eaf07407d2bcba899deb94e7dbf14ab0e4e427df
brunoziie <eu@brunoziie.com>
2019-11-21 11:28:48 -0300

    remove resources generated in tests.

--- src/app/providers/api/sessions.js
+++
@@ -1,8 +0,0 @@
-class SessionsResource extends RESTFullResource {
-    static endpoint() {
-        return 'sessions';
-    }
-    static store(data) {
-        return this.request('POST', '/', data);
-    }
-}
