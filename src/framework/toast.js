4540c7acde39c75fbce368005f4e79f7279bb15a
Lizandra Lígia <lizandraligia@hotmail.com>
2020-03-19 09:26:32 -0300

    correção na identação

--- src/framework/toast.js
+++ src/framework/toast.js
@@ -1,25 +1,25 @@
 (function() {
-	const getHostElement = () => {
-		let host = document.querySelector('.sh-toast-host');
+    const getHostElement = () => {
+        let host = document.querySelector('.sh-toast-host');
 
-		if (!host) {
-			const el = document.createElement('div');
-			el.classList.add('sh-toast-host');
-			document.body.appendChild(el);
-			host = el;
-		}
+        if (!host) {
+            const el = document.createElement('div');
+            el.classList.add('sh-toast-host');
+            document.body.appendChild(el);
+            host = el;
+        }
 
-		return host;
-	}
+        return host;
+    }
 
-	const createToast = (message, type) => {
-		const el = document.createElement('div');
-		el.classList.add('sh-toast');
-		el.classList.add(`sh-toast-${type}`.toLowerCase());
-		el.innerHTML = `<div class="sh-toast-content">${renderMessage(message)}</div>`;
+    const createToast = (message, type) => {
+        const el = document.createElement('div');
+        el.classList.add('sh-toast');
+        el.classList.add(`sh-toast-${type}`.toLowerCase());
+        el.innerHTML = `<div class="sh-toast-content">${renderMessage(message)}</div>`;
 
-		return el;
-	}
+        return el;
+    }
 
     const renderMessage = (message) => {
         return String(message)
@@ -30,23 +30,23 @@
             .replace(/\*\*(.*?)\*\*/gm, '<b>$1</b>');
     }
 
-	function toast (message, type) {
-		const el = createToast(message, type || toast.SUCCESS);
-		getHostElement().appendChild(el);
+    function toast (message, type) {
+        const el = createToast(message, type || toast.SUCCESS);
+        getHostElement().appendChild(el);
 
-		setTimeout(() => {
-			el.classList.add('visible');
-		}, 50);
+        setTimeout(() => {
+            el.classList.add('visible');
+        }, 50);
 
-		setTimeout(() => {
-			el.classList.remove('visible');
-			setTimeout(() => getHostElement().removeChild(el), 500);
-		}, 5000);
-	}
+        setTimeout(() => {
+            el.classList.remove('visible');
+            setTimeout(() => getHostElement().removeChild(el), 500);
+        }, 5000);
+    }
 
-	toast.SUCCESS = 'SUCCESS';
-	toast.ERROR = 'ERROR';
-	toast.INFO = 'INFO';
+    toast.SUCCESS = 'SUCCESS';
+    toast.ERROR = 'ERROR';
+    toast.INFO = 'INFO';
 
-	window.toast = toast;
+    window.toast = toast;
 })();
