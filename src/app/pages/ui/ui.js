df7cc6d07c2744fc8bdee34477164e43f2e2c4d6
Lizandra Lígia <lizandraligia@hotmail.com>
2020-04-09 14:18:02 -0300

    adição de componentes na ui page e correção em input

--- src/app/pages/ui/ui.js
+++ src/app/pages/ui/ui.js
@@ -5,12 +5,34 @@ Page('ui', {
         return {
             loading: true,
             sending: false,
-            resource: {},
+            resource: {
+                'default': null,
+                primary: null,
+                success: null,
+                info: null,
+                warning: null,
+                danger: null,
+                small: null,
+                medium: null,
+                large: null,
+                rounded: null
+            },
+
             datasources: {}
         }
     },
 
-    methods: {},
+    methods: {
+        onPaginate (page) {
+            const params = {
+                query: {
+                    page
+                }
+            };
+            this.$router.push(params, () => this.loadResources(page));
+        }
+    },
+
     async mounted () {
         this.loading = false;
     }
